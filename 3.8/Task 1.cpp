4f023b90ebf24f1cff81bff0f2a2be397a7bbc83
FerociousRabbit <Sintel2016@gmail.com>
2022-11-25 10:25:57 +0300

    added homework 3.8

---
+++ 3.8/Task 1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, max = 0, min = 0, sum = 0, a = 0;
+    printf("Укажите количество чисел, которое хотите ввести:");
+    scanf("%i", &i);
+
+    for (int j = 0; j < i; j++)
+    {
+        printf("Введите число:");
+        scanf("%i", &a);
+
+        sum = sum + a;
+
+        if (max < a)
+        {
+            max = a;
+        }
+        if (min == 0)
+        {
+            min = a;
+        }
+        else if (min > a)
+        {
+            min = a;
+        }
+    }
+
+    printf("Сумма чисел = %i\n", sum);
+    printf("Среднее значение = %i\n", sum / i);
+    printf("Максимальное число = %i\n", max);
+    printf("Минимальное число = %i\n", min);
+}
